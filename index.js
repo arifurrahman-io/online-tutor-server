81f1e8f26fbc6d83ce2be149cb37cdbefcf94605
arifurrahman01 <arifurrahman.now@gmail.com>
2022-10-25 16:41:06 +0600

    data fetching method added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const courses = require('./data/data.json');
 
@@ -10,7 +13,24 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courses);
-})
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const getSingleCourse = courses?.find((course) => course.id == id);
+    if (!getSingleCourse) {
+        res.send('Data not found!');
+    }
+    res.send(getSingleCourse);
+});
+app.get('/category/:name', (req, res) => {
+    const name = req.params.name;
+    const getCategory = courses?.filter((c) => c.category == name);
+    if (!getCategory) {
+        res.send('Unavailable Category!');
+    }
+    res.send(getCategory);
+});
 
 app.listen(port, () => {
     console.log('Server PORT 5000 Running')
