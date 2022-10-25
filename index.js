0c9a1f4dc6f5c74591e512ed04613ff5392da479
arifurrahman01 <arifurrahman.now@gmail.com>
2022-10-25 14:10:57 +0600

    data file added

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/data.json');
+
 app.get('/', (req, res) => {
-    res.send('Server Running');
+    res.send('Server is Running');
 });
 
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
 app.listen(port, () => {
-    console.log('Server running port 5000')
+    console.log('Server PORT 5000 Running')
 });
